455d2ecf0e974db98c1976357681f245a6a48e4f
Hemang-devlops <136294289+Hemang-devlops@users.noreply.github.com>
2024-05-20 10:16:27 +0530

    Initial commit

---
+++ src/data/folderData.js
@@ -0,0 +1,32 @@
+const explorer = {
+  id: '1',
+  name: 'root',
+  isFolder: true,
+  items: [
+    {
+      id: '2',
+      name: 'public',
+      isFolder: true,
+      items: [
+        {
+          id: '3',
+          name: 'public nested 1',
+          isFolder: false,
+          items: []
+        },
+        {
+          id: '4',
+          name: 'public nested 2',
+          isFolder: false,
+          items: []
+        },
+      ]
+    },
+    {
+      id: '5',
+      name: 'src',
+      isFolder: true,
+      items: []
+    }
+  ]
+};
